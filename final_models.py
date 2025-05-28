be21774e2386cff694691e178bd5a6b789b9569e
Gellert Toth <stgellertg@gmail.com>
2025-05-28 15:25:26 +0200

    update final models

--- final_models.py
+++ final_models.py
@@ -1,16 +1,28 @@
 final_models = {
     "A": [
-        "./checkpoints/model_A_round_9_f1_0.6882_best.pth",
-        "./checkpoints/model_A_round_10_f1_0.6902_best.pth"
+        # "./checkpoints/model_A_round_9_f1_0.6882_best.pth",
+        # "./checkpoints/model_A_round_10_f1_0.6902_best.pth",
+        "./checkpoints/model_A_round_20_f1_0.6975_best.pth",
+        "./checkpoints/model_A_round_21_f1_0.7135_best.pth",
+        "./checkpoints/model_A_round_22_f1_0.7005_best.pth",
+
     ],
     "B": [
-        "./checkpoints/model_B_round_9_f1_0.5617_best.pth",
-        "./checkpoints/model_B_round_10_f1_0.5354_best.pth",
+        # "./checkpoints/model_B_round_9_f1_0.5617_best.pth",
+        # "./checkpoints/model_B_round_10_f1_0.5354_best.pth",
+        "./checkpoints/model_B_round_20_f1_0.5495_best.pth",
+        "./checkpoints/model_B_round_21_f1_0.5250_best.pth",
+        "./checkpoints/model_B_round_22_f1_0.5397_best.pth",
+
 
     ],
     "C": [
-        "./checkpoints/model_C_round_9_f1_0.8179_best.pth",
-        "./checkpoints/model_C_round_10_f1_0.8192_best.pth",
+        # "./checkpoints/model_C_round_9_f1_0.8179_best.pth",
+        # "./checkpoints/model_C_round_10_f1_0.8192_best.pth",
+        "./checkpoints/model_C_round_20_f1_0.8175_best.pth",
+        "./checkpoints/model_C_round_21_f1_0.8151_best.pth",
+        "./checkpoints/model_C_round_22_f1_0.8346_best.pth",
+
     ],
     "D": [
         "./checkpoints/model_D_round_9_f1_0.7486_best.pth",
