ad2d600c0b3c6245e05ab73d603c90a172a809fc
Gellert Toth <stgellertg@gmail.com>
2025-05-28 12:30:01 +0200

    training model 9 D

--- final_models.py
+++ final_models.py
@@ -5,11 +5,16 @@ final_models = {
     ],
     "B": [
         "./checkpoints/model_B_round_9_f1_0.5617_best.pth",
+        "./checkpoints/model_B_round_10_f1_0.5354_best.pth",
+
     ],
     "C": [
         "./checkpoints/model_C_round_9_f1_0.8179_best.pth",
+        "./checkpoints/model_C_round_10_f1_0.8192_best.pth",
     ],
     "D": [
-        "./checkpoints/model_C_round_9_f1_0.5617_best.pth",
+        "./checkpoints/model_D_round_9_f1_0.7486_best.pth",
+        "./checkpoints/model_D_round_10_f1_0.7647_best.pth",
+
     ]
 }
