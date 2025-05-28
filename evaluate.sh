f7b89f34bdbd5a85280815e7ee751c9e640a0739
Gellert Toth <stgellertg@gmail.com>
2025-05-28 09:12:55 +0200

    update eval

--- evaluate.sh
+++ evaluate.sh
@@ -2,5 +2,5 @@ source .venv/bin/activate
 for dataset in A B C D; do
     python3 main_vgae.py --train_path ./datasets/A/train.json.gz \
         --test_path ./datasets/$dataset/test.json.gz \
-        --models "./checkpoints/model_${dataset}_0_best.pth ./checkpoints/model_${dataset}_1_best.pth ./checkpoints/model_${dataset}_2_best.pth ./checkpoints/model_${dataset}_3_best.pth ./checkpoints/model_${dataset}_4_best.pth ./checkpoints/model_${dataset}_5_best.pth ./checkpoints/model_${dataset}_6_best.pth ./checkpoints/model_${dataset}_7_best.pth ./checkpoints/model_${dataset}_6_best.pth ./checkpoints/model_${dataset}_8_best.pth"
+        --models "./checkpoints/model_${dataset}_0_best.pth ./checkpoints/model_${dataset}_1_best.pth ./checkpoints/model_${dataset}_2_best.pth ./checkpoints/model_${dataset}_3_best.pth ./checkpoints/model_${dataset}_4_best.pth ./checkpoints/model_${dataset}_5_best.pth ./checkpoints/model_${dataset}_6_best.pth ./checkpoints/model_${dataset}_7_best.pth ./checkpoints/model_${dataset}_8_best.pth"
 done
